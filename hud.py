6d040c586ec6a5a47c8bfaddf35fe1f31f1495ed
Nerubes <gurov.no@phystech.edu>
2021-04-24 15:28:05 +0300

    описания(часть)

--- hud.py
+++ hud.py
@@ -12,6 +12,9 @@ wave = None
 
 
 def update():
+    """
+    Обновляет значение hp, coin, wave
+    """
     global hp, coin, wave
     hp = font.render(str(global_names.CASTLE.hp), True, global_names.WHITE)
     coin = font.render(str(global_names.CASTLE.money), True,
--- saver.py
+++ saver.py
@@ -5,6 +5,9 @@ import global_names
 
 
 def load():
+    """
+    Загружает из файла уже созданные карты
+    """
     if os.path.exists(global_names.MY_PATH) and os.path.getsize(
             global_names.MY_PATH) > global_names.EMPTY:
         with open(global_names.MY_PATH, "rb") as f:
@@ -12,6 +15,11 @@ def load():
 
 
 def convert():
+    """
+    Переводит карту под номером TEMP_ID из массива карт из формата массива чисел,
+    в котором они сохраняются в массив картинок
+    :return: массив картинок, отображающий карту
+    """
     data = []
     for temp_y in range(global_names.MAP.width):
         temp_data = []
@@ -24,6 +32,9 @@ def convert():
 
 
 def save_after_editor():
+    """
+    Добавляет только что созданную карту к тем, что уже были и сохраняет их все в файл
+    """
     with open("maps/maps.pickle", "wb") as f:
         data = []
         for temp_y in range(global_names.MAP.width):
@@ -37,5 +48,8 @@ def save_after_editor():
 
 
 def save():
+    """
+    Сохраняет карты в файл
+    """
     with open("maps/maps.pickle", "wb") as f:
         pickle.dump(global_names.MAPS_COLLECTION, f)
